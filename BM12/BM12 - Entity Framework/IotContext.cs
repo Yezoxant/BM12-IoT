2eb5bfe13a9fc43d329c58b7401aa46dc77b8f7e
Brandon <brandon94kleijnen@live.nl>
2018-01-24 14:58:16 +0100

    Change EF

---
+++ BM12/BM12 - Entity Framework/IotContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BM12___Entity_Framework
+{
+    public class IotContext: DbContext
+    {
+
+    }
+}
---
+++ BM12/BM12.Data/IotContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BM12___Entity_Framework.Models;
+
+namespace BM12___Entity_Framework
+{
+    public class IotContext: DbContext
+    {
+        public IotContext(DbContextOptions<IotContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
