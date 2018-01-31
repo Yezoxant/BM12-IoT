2a7205ae6464843f894d559038fd3eb25a42e724
HSZUYD\1439235houben <cedric.houben@live.nl>
2018-01-31 18:42:00 +0100

    added post picture endpoint

--- BM12/BM12/Controllers/PictureController.cs
+++ BM12/BM12/Controllers/PictureController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BM12;
 using BM12.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Net.Http;
 
 namespace BM12___Webapplication.Controllers
 {
@@ -15,112 +19,38 @@ namespace BM12___Webapplication.Controllers
     public class PictureController : Controller
     {
         private readonly IotContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public PictureController(IotContext context)
+        public PictureController(IotContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
-
-        // GET: api/Picture
-        [HttpGet]
-        public IEnumerable<PictureData> GetPictureData()
-        {
-            return _context.PictureData;
-        }
-
-        // GET: api/Picture/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetPictureData([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var pictureData = await _context.PictureData.SingleOrDefaultAsync(m => m.Id == id);
-
-            if (pictureData == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(pictureData);
-        }
-
-        // PUT: api/Picture/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPictureData([FromRoute] int id, [FromBody] PictureData pictureData)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != pictureData.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(pictureData).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PictureDataExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
+        
 
         // POST: api/Picture
         [HttpPost]
-        public async Task<IActionResult> PostPictureData([FromBody] PictureData pictureData)
+        public async Task<IActionResult> PostPictureData([FromBody] PictureDto pictureData)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+            //System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            //var id = _userManager.GetUserId(User);
 
-            _context.PictureData.Add(pictureData);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPictureData", new { id = pictureData.Id }, pictureData);
-        }
-
-        // DELETE: api/Picture/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePictureData([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var pictureData = await _context.PictureData.SingleOrDefaultAsync(m => m.Id == id);
-            if (pictureData == null)
-            {
-                return NotFound();
-            }
-
-            _context.PictureData.Remove(pictureData);
+            await _context.PictureData.AddAsync(new PictureData { Attention = pictureData.Attention, DateTime = DateTime.Now, Emotion = pictureData.Emotion, UserId = 3});
             await _context.SaveChangesAsync();
 
             return Ok(pictureData);
         }
 
-        private bool PictureDataExists(int id)
+
+        public class PictureDto
         {
-            return _context.PictureData.Any(e => e.Id == id);
+            public string Emotion { get; set; }
+      
+            public string Attention { get; set; }
         }
     }
 }
